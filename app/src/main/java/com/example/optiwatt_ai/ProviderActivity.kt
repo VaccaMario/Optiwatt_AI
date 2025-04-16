fa42f08c34bde59885d9dfe0ab79c0687e009fca
VaccaMario <mario.vacca002@studenti.uniparthenope.it>
2025-04-16 12:44:25 +0200

    aggiunta delle schermate iniziali, schermata di apertura dell'app e schermata per collegare il provider.

--- app/src/main/java/com/example/optiwatt_ai/ProviderActivity.kt
+++ app/src/main/java/com/example/optiwatt_ai/ProviderActivity.kt
@@ -1,31 +1,20 @@
 package com.example.optiwatt_ai
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ProviderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val mainLayout = findViewById<View>(R.id.main)
-        ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
+        setContentView(R.layout.activity_provider)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
-        getStartedButton.setOnClickListener {
-            val intent = Intent(this, ProviderActivity::class.java)
-            startActivity(intent)
         }
     }
 }
