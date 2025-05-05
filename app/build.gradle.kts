dfa2df0929fe3d025a00429083a07c3290c27f29
VaccaMario <mario.vacca002@studenti.uniparthenope.it>
2025-05-05 16:29:37 +0200

    aggiunta build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,5 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.activity:activity-ktx:1.8.0")
-
+    implementation("androidx.room:room-ktx:2.6.1")
 }
