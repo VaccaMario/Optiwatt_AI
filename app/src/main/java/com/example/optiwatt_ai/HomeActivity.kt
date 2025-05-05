2453065b14357bd036e64258bd50fcbe5811af7e
Rocco4455 <rocco.rizzitano001@studenti.uniparthenope.it>
2025-05-05 13:24:36 +0200

    aggiornata immagine uploadicon

--- app/src/main/java/com/example/optiwatt_ai/HomeActivity.kt
+++ app/src/main/java/com/example/optiwatt_ai/HomeActivity.kt
@@ -4,12 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.PopupMenu
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.LayoutInflaterCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.view.LayoutInflater
